afe65b5d1a1dfb8b0a56d706244d11bb56e0f3d0
zohaf <zohafard@gmail.com>
2022-10-05 14:14:13 +0200

    test

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 const express = require("express");
-const path = require("path");
-const pa11y = require("pa11y");
-const PORT = process.env.PORT || 4000;
-
 const app = express();
-const { v4 } = require("uuid");
+const path = require("path");
 
 app.use(express.static("public"));
 
-app.get("/api/test", async (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile("index.html", { root: path.join(__dirname, "public") });
-  if (!req.query.url) {
-    res.status(400).json({ error: "url is required" });
-  } else {
-    const results = await pa11y(req.query.url);
-    res.status(200).json(results);
-  }
 });
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(process.env.PORT || 3000);
 
 module.exports = app;

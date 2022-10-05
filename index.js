0716f50bda993fd8898e3da8a879796c4b629be9
zohaf <zohafard@gmail.com>
2022-10-05 14:04:31 +0200

    update server to deploy to vercel

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
+const path = require("path");
 const pa11y = require("pa11y");
 const PORT = process.env.PORT || 4000;
 
 const app = express();
+const { v4 } = require("uuid");
 
 app.use(express.static("public"));
 
 app.get("/api/test", async (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
   if (!req.query.url) {
     res.status(400).json({ error: "url is required" });
   } else {
@@ -16,3 +19,5 @@ app.get("/api/test", async (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+module.exports = app;

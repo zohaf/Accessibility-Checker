c8a7ee3a1daaf0c8810a4d6948972f2818ec0336
zohaf <zohafard@gmail.com>
2022-10-05 14:20:41 +0200

    update indexjs

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const pa11y = require("pa11y");
+const PORT = process.env.PORT || 4000;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.sendFile("index.html", { root: path.join(__dirname, "public") });
+
+  if (!req.query.url) {
+    res.status(400).json({ error: "url is required" });
+  } else {
+    const results = await pa11y(req.query.url);
+    res.status(200).json(results);
+  }
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 module.exports = app;
